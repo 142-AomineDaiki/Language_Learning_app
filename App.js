d4cff41367df458bba67772e920b6bfeaddb4930
SujithClasher <113553567+SujithClasher@users.noreply.github.com>
2023-10-07 06:55:31 +0530

    updated

--- App.js
+++ App.js
@@ -1,20 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Image, StyleSheet, Text, TouchableOpacity } from 'react-native';
 
-export default function App() {
+const YourComponent = () => {
+  const [loaded, setLoaded] = useState(false);
+  const [showNextPage, setShowNextPage] = useState(false);
+
+  const handleButtonPress = () => {
+    // When the button is pressed, toggle the state to show the next page component
+    setShowNextPage(true);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {showNextPage ? (
+        <NextPageComponent />
+      ) : (
+        <>
+          <Image
+            source={require('./assets/duolingo-opp.gif')}
+            style={[styles.gif, loaded && styles.loaded]}
+            resizeMode="contain"
+            onLoad={() => setLoaded(true)}
+          />
+          {loaded && (
+            <View style={styles.textContainer}>
+              <Text style={styles.text}>Pico</Text>
+            </View>
+          )}
+
+          {/* Add a button to navigate to the next page */}
+          <TouchableOpacity style={styles.button} onPress={handleButtonPress}>
+            <Text style={styles.buttonText}>Go to Next Page</Text>
+          </TouchableOpacity>
+        </>
+      )}
     </View>
   );
-}
+};
+
+// Define the NextPageComponent that will be shown when the button is pressed
+const NextPageComponent = () => {
+  return (
+    <View style={styles.container}>
+      {/* Content for the next page */}
+      <Text style={styles.text}>This is the Next Page</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'navy',
+    position: 'relative',
+  },
+  gif: {
+    width: 200,
+    height: 200,
+    opacity: 0,
+  },
+  loaded: {
+    opacity: 1,
+  },
+  textContainer: {
+    position: 'absolute',
+    bottom: 20,
+  },
+  text: {
+    color: 'white',
+    fontSize: 30,
+    marginBottom: 30,
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
   },
 });
+
+export default YourComponent;
